1305c4198e453eb41b323445a02cf080a1333786
chris_swindell <chris@the-tardis.Home>
2020-02-01 00:09:37 -0600

    Added player ship movement commands

---
+++ Assets/Scripts/ship_movements.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship_movements : MonoBehaviour
+{
+    public float moveSpeed;
+    public float thrustSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+	moveSpeed = 5f;
+	thrustSpeed = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+
+	if (Input.GetKeyDown(KeyCode.W))
+	{
+	    this.GetComponent<Rigidbody>().AddForce(transform.forward*thrustSpeed);
+	}
+    }
+}
