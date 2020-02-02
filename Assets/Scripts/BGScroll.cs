70cf95efe678b3ebbc2f7dc34414dfb8d6f2fcf2
Jarod Teague <jarodteague@yahoo.com>
2020-02-02 01:03:18 -0600

    added Parrallax Background

---
+++ Assets/Scripts/BGScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRenderer thisMesh = GetComponent<MeshRenderer>();
+
+        Material Mat_1 = thisMesh.materials[0];
+
+        Vector2 offset_1 = Mat_1.mainTextureOffset;
+
+        offset_1.y += Time.deltaTime / 20;
+
+        Mat_1.mainTextureOffset = offset_1;
+    }
+}
