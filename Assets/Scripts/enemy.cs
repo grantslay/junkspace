57d52b14267e9d9a0ed504bdd87c6dc2c750763b
chris_swindell <chris@the-tardis.Home>
2020-02-01 19:51:19 -0600

    Added junk spawn on enemy health reaching zero giving each piece of junk a random z-axis displacement and y-axis rotation.

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -1,24 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
-
+/*
     public GameObject target;
     public GameObject laser;
     public float speed = 2;
-    public float shootTimer = 3;
+    public float shootTimer = 3;*/
+    public float health;
+    public GameObject Junk_01;
+    public GameObject Junk_02;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       health = 0.0f; 
     }
 
     // Update is called once per frame
     void Update()
     {
+	    /*
         Vector3 targetDirection = target.transform.position - transform.position;
         float rotateStep = speed * Time.deltaTime;
         Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, rotateStep, 0.0f);
@@ -41,6 +45,48 @@ public class enemy : MonoBehaviour
                 shootTimer = 3;
             }
         }
+*/
+		if (health == 0.0)
+		{
+			spawn_junk();
+			Destroy(this.gameObject);
+		}
+    }
 
+    void spawn_junk()
+    {
+		//get current position
+		Vector3 curr_pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+		
+		//generate required random values for junk
+		System.Random r = new System.Random();
+		int junk_type = r.Next(1,3);
+		int num_junk = r.Next(1,5);
+		
+		//release junk onto current position
+		if (junk_type == 1)
+		{
+			Quaternion junk_rot = Quaternion.identity;
+			
+			for (int i = 1; i <= num_junk; i++)
+			{
+				int junk_travel_dist = r.Next(1,16);
+				junk_rot.eulerAngles = new Vector3(0.0f, r.Next(0,360), 0.0f);
+				Instantiate(Junk_01, new Vector3(curr_pos.x, curr_pos.y, curr_pos.z + junk_travel_dist), junk_rot);
+			}
+		}
+		
+		else
+		{
+			Quaternion junk_rot = Quaternion.identity;
+			
+			for (int i = 1; i <= num_junk; i++)
+			{
+				int junk_travel_dist = r.Next(1,16);
+				junk_rot.eulerAngles = new Vector3(0.0f, r.Next(0,360), 0.0f);
+				Instantiate(Junk_02, new Vector3(curr_pos.x, curr_pos.y, curr_pos.z + junk_travel_dist), junk_rot);
+			}
+		}
+		//send junk in random direction
     }
 }
